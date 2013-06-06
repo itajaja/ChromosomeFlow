a06f65cf550b394df2a4a329bb611374861b3564
Giacomo <giacomo.tag@gmail.com>
2013-06-07 00:56:48 +0200

    added test level

--- ChromosomeFlow/Main.cs
+++ ChromosomeFlow/Main.cs
@@ -8,7 +8,7 @@ namespace test
 		{
 			Console.WriteLine ("parsing the file...");
 			LevelParser parser = new LevelParser();
-			parser.ParseFile(System.IO.File.ReadAllLines(@"testlevel"));
+			parser.ParseFile(System.IO.File.ReadAllLines(@"../../testlevel1.cf"));
 			Console.WriteLine("parsing successfull");
 		}
 	}
